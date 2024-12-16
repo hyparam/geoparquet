336aad881037d9dd35d6e270252a6fe24932c761
Kenny Daniel <platypii@gmail.com>
2024-12-16 11:37:53 -0800

    Add GeoJSON types

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 
-export function geoparquet2geojson(asyncBuffer: AsyncBuffer) {
-
+/**
+ * @import { AsyncBuffer } from 'hyparquet'
+ * @import { GeoJSON } from './geojson.js'
+ * @param {AsyncBuffer} asyncBuffer 
+ * @returns {GeoJSON}
+ */
+export function geoparquet2geojson(asyncBuffer) {
+  return {
+    type: 'FeatureCollection',
+    features: [],
+  }
 }
