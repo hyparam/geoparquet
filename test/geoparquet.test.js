e6ab017917f4262600a3c15b1ddc638a5bb18805
Kenny Daniel <platypii@gmail.com>
2024-12-16 16:56:12 -0800

    Add polygon test

--- test/geoparquet.test.js
+++ test/geoparquet.test.js
@@ -1,12 +1,19 @@
 import { asyncBufferFromFile } from 'hyparquet'
 import { describe, expect, it } from 'vitest'
 import { toGeoJson } from '../src/index.js'
-import expected from '../examples/example.json' with { type: 'json' }
+import example from '../examples/example.json' with { type: 'json' }
+import polys from '../examples/polys.json' with { type: 'json' }
 
 describe('geoparquet', () => {
-  it('should parse example geoparquet file', async () => {
+  it('should parse example.parquet', async () => {
     const asyncBuffer = await asyncBufferFromFile('examples/example.parquet')
     const geojson = await toGeoJson(asyncBuffer)
-    expect(geojson).toEqual(expected)
+    expect(geojson).toEqual(example)
+  })
+
+  it('should parse polys.parquet', async () => {
+    const asyncBuffer = await asyncBufferFromFile('examples/polys.parquet')
+    const geojson = await toGeoJson(asyncBuffer)
+    expect(geojson).toEqual(polys)
   })
 })
